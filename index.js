6c0a03694f60d7e31f73bba8d7688bae83da8181
Sandra Katsikis <mindfulripples@gmail.com>
2023-08-17 21:39:33 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(finish){
+    let numberOfBlocks = (Math.abs(finish - 42));
+    return(numberOfBlocks);
+}
+
+function distanceFromHqInFeet(finish){
+    let feetTravelled = 264 * distanceFromHqInBlocks(finish);
+    return(feetTravelled);
+}
+
+function distanceTravelledInFeet(start, finish){
+    let distance = 264 * (Math.abs(finish - start));
+    return(distance);
+}
+
+function calculatesFarePrice(start, finish){
+    let distance = distanceTravelledInFeet(start, finish);
+    if (distance <= 400){
+        return(0);
+      } else if (distance <= 2000){
+        return(0.02 * (distance - 400));
+      } else if (distance <= 2500){
+        return(25);
+      } else {
+         return('cannot travel that far')
+      }
+}
